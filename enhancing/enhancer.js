362db8689f273e5c0ad7a02202a45484a56a667c
Matthew Martin <zhintateo19924559@gmail.com>
2021-02-13 17:37:03 -0600

    Test Passed and code works

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,47 @@
 module.exports = {
-  success,
-  fail,
-  repair,
-  get,
+	succeed,
+	fail,
+	repair,
+	get,
 };
 
-function success(item) {
-  return { ...item };
+function succeed(item) {
+	succeededItem = item;
+	if (succeededItem.enhancement > 19)
+		return { ...succeededItem
+		};
+	else return { ...succeededItem,
+		enhancement: succeededItem.enhancement + 1
+	}
 }
 
 function fail(item) {
-  return { ...item };
+	failedItem = item;
+	if (failedItem.enhancement < 15) {
+		return { ...failedItem,
+			durability: failedItem.durability - 5
+		}
+	} else if (failedItem.enhancement === 15 || failedItem.enhancement === 16) {
+		return { ...failedItem,
+			durability: failedItem.durability - 10
+		}
+	} else if (failedItem.enhancement > 16) {
+		return { ...failedItem,
+			durability: failedItem.durability - 10,
+			enhancement: failedItem.enhancement - 1
+		}
+	}
+	return { ...item
+	};
 }
 
 function repair(item) {
-  return { ...item };
+	return { ...item,
+		durability: 100
+	};
 }
 
 function get(item) {
-  return { ...item };
+	return { ...item
+	};
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,79 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const {
+	repair,
+	succeed,
+	fail,
+	get
+} = require('./enhancer.js');
+
+describe('enhancement functions', () => {
+
+	describe('succeed()', () => {
+		it('should increase enhancement by 1 unless enhancement is already 20', () => {
+			const item = {
+				name: "Axe",
+				durability: 75,
+				enhancement: 15
+			};
+			const masterItem = {
+				name: "Axe",
+				durability: 75,
+				enhancement: 20
+			};
+
+			expect(succeed(item)).toEqual({
+				name: "Axe",
+				durability: 75,
+				enhancement: 16
+			});
+			expect(succeed(masterItem)).toEqual(masterItem);
+		});
+	});
+
+	describe('repair()', () => {
+		it("should return an item with durability: 100", () => {
+			const item = {
+				name: "Shield",
+				durability: 50
+			};
+
+			expect(repair(item)).toEqual({
+				name: "Shield",
+				durability: 100
+			});
+		});
+	});
+
+	describe("fail()", () => {
+		it("shouuld decrease enhancement by one and decrease durabiility by 10 when enhancement is over 16", () => {
+			const item = {
+				name: "sword",
+				durability: 80,
+				enhancement: 20,
+			};
+
+			const expected = {
+				name: "sword",
+				durability: 70,
+				enhancement: 19,
+			};
+
+			expect(fail(item)).toEqual(expected);
+		});
+
+		it("should should decrease durability by 5 when enhancement is less than 15", () => {
+			const item = {
+				name: "sword",
+				durability: 80,
+				enhancement: 14,
+			};
+
+			const expected = {
+				name: "sword",
+				durability: 75,
+				enhancement: 14,
+			};
+
+			expect(fail(item)).toEqual(expected);
+		});
+	});
+});
